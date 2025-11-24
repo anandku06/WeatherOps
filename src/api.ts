497eefca5a2ed85415a5b309384add31b666f34f
Anand Kumar <rishavgupta163@gmail.com>
2025-11-24 10:40:52 +0530

    fix: correct API endpoint for weather data fetching

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 const API_key = import.meta.env.VITE_WEATHER_API_KEY
 
 export async function getWeather({lat, long} : {lat: number, long: number}) {
-    const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely,alerts&appid=${API_key}`)
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,alerts&appid=${API_key}`)
 
     return res.json()
 }
