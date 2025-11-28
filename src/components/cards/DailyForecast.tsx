e92fb9b41fb53d4c80eb53d417277f7011a684f4
Anand Kumar <rishavgupta163@gmail.com>
2025-11-28 20:01:20 +0530

    feat: enhance DailyForecast component to utilize Card's childrenClassName prop for styling

--- src/components/cards/DailyForecast.tsx
+++ src/components/cards/DailyForecast.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "./Card";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { getWeather } from "../../api";
@@ -13,25 +12,23 @@ const DailyForecast = (props: Props) => {
 
   return (
     <>
-      <Card title="Daily Forecast">
-        <div className="flex flex-col gap-4">
-          {data?.daily.map((day) => (
-            <div key={day.dt} className="flex justify-between">
-              <p className="w-9">
-                {new Date(day.dt * 1000).toLocaleDateString(undefined, {
-                  weekday: "short",
-                })}
-              </p>
-              <img
-                src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                alt="Weather Icon"
-              />
-              <p>{Math.round(day.temp.day)}°C</p>
-              <p className="text-gray-500/75">{Math.round(day.temp.max)}°C</p>
-              <p className="text-gray-500/75">{Math.round(day.temp.min)}°C</p>
-            </div>
-          ))}
-        </div>
+      <Card title="Daily Forecast" childrenClassName="flex flex-col gap-4">
+        {data?.daily.map((day) => (
+          <div key={day.dt} className="flex justify-between">
+            <p className="w-9">
+              {new Date(day.dt * 1000).toLocaleDateString(undefined, {
+                weekday: "short",
+              })}
+            </p>
+            <img
+              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+            <p>{Math.round(day.temp.day)}°C</p>
+            <p className="text-gray-500/75">{Math.round(day.temp.max)}°C</p>
+            <p className="text-gray-500/75">{Math.round(day.temp.min)}°C</p>
+          </div>
+        ))}
       </Card>
     </>
   );
