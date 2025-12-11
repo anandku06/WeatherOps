ad552d3874802820c80e17743e0b3390dc483d74
Anand Kumar <rishavgupta163@gmail.com>
2025-12-11 23:21:34 +0530

    feat: refactor coordinate handling and integrate geocoding for dynamic location updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,39 @@ import Map from "./components/Map";
 import { useState } from "react";
 import type { Coords } from "./types";
 import LocationDropdown from "./components/dropdowns/LocationDropdown";
+import { useQuery } from "@tanstack/react-query";
+import { getGeoCode } from "./api";
 
 function App() {
-  const [coords, setCoords] = useState<Coords>({
+  const [coordinates, setCoordinates] = useState<Coords>({
     lat: 37.7749,
     long: -122.4194,
   });
 
+  const [location, setLocation] = useState<string>("Tokyo");
+  const { data } = useQuery({
+    queryKey: ["geocode", location],
+    queryFn: () => getGeoCode(location),
+  });
+
   const onMapClick = (newCoords: Coords) => {
-    setCoords(newCoords);
+    setCoordinates(newCoords);
+    setLocation("custom");
   };
+
+  const coords =
+    location === "custom"
+      ? coordinates
+      : { lat: data?.[0].lat ?? 0, long: data?.[0].lon ?? 0 };
 
   return (
     <div className="flex flex-col gap-8">
       <LocationDropdown />
-      <Map coords={coords} onMapClick={onMapClick} />
-      <CurrentWeather coords={coords} />
-      <HourlyForecast coords={coords} />
-      <DailyForecast coords={coords} />
-      <AdditionalInfo coords={coords} />
+      <Map coords={coordinates} onMapClick={onMapClick} />
+      <CurrentWeather coords={coordinates} />
+      <HourlyForecast coords={coordinates} />
+      <DailyForecast coords={coordinates} />
+      <AdditionalInfo coords={coordinates} />
     </div>
   );
 }
