a2d884d92e6d8e5bf547856e42174b845fde51ba
Anand Kumar <rishavgupta163@gmail.com>
2025-12-22 13:27:05 +0530

    feat: update MapTypeDropdown to improve code consistency and formatting

--- src/components/dropdowns/MapTypeDropdown.tsx
+++ src/components/dropdowns/MapTypeDropdown.tsx
@@ -1,32 +1,32 @@
-import type { Dispatch, SetStateAction } from "react"
+import type { Dispatch, SetStateAction } from "react";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "../ui/select"
+} from "../ui/select";
 
 type Props = {
-  mapType: string
-  setMapType: Dispatch<SetStateAction<string>>
-}
+  mapType: string;
+  setMapType: Dispatch<SetStateAction<string>>;
+};
 
 export default function MapTypeDropdown({ mapType, setMapType }: Props) {
   return (
     <Select value={mapType} onValueChange={(value) => setMapType(value)}>
-      <SelectTrigger className="w-full xs:w-[180px]">
+      <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Types" />
       </SelectTrigger>
       <SelectContent className="z-1001">
         {types.map((city, i) => (
           <SelectItem key={i} value={city}>
-            {city}
+            {city.split("_")[0]}
           </SelectItem>
         ))}
       </SelectContent>
     </Select>
-  )
+  );
 }
 
 const types = [
@@ -35,4 +35,4 @@ const types = [
   "pressure_new",
   "wind_new",
   "temp_new",
-]
+];
