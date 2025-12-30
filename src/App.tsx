2e97a7eb6adb7bf909372b4bc74feccb1ccc6dc5
Anand Kumar <rishavgupta163@gmail.com>
2025-12-30 17:41:38 +0530

    feat: implement loading skeletons for weather data components and add fade-in animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { getGeoCode } from "./api";
 import AdditionalInfo from "./components/cards/AdditionalInfo";
 import CurrentWeather from "./components/cards/CurrentWeather";
 import DailyForecast from "./components/cards/DailyForecast";
 import HourlyForecast from "./components/cards/HourlyForecast";
 import LocationDropdown from "./components/dropdowns/LocationDropdown";
-import Map from "./components/Map";
-import type { Coords } from "./types";
 import MapTypeDropdown from "./components/dropdowns/MapTypeDropdown";
+import Map from "./components/Map";
 import MapLegend from "./components/MapLegend";
+import CurrentSkeleton from "./components/skeletons/CurrentSkeleton";
+import DailySkeleton from "./components/skeletons/DailySkeleton";
+import HourlySkeleton from "./components/skeletons/HourlySkeleton";
+import type { Coords } from "./types";
+import AdditionalInfoSkeleton from "./components/skeletons/AdditionalInfoSkeleton";
 
 function App() {
   const [coordinates, setCoordinates] = useState<Coords>({ lat: 0, long: 0 });
@@ -48,10 +52,18 @@ function App() {
         <Map coords={coords} onMapClick={onMapClick} mapType={mapType} />
         <MapLegend mapType={mapType} />
       </div>
-      <CurrentWeather coords={coords} />
-      <HourlyForecast coords={coords} />
-      <DailyForecast coords={coords} />
-      <AdditionalInfo coords={coords} />
+      <Suspense fallback={<CurrentSkeleton />}>
+        <CurrentWeather coords={coords} />
+      </Suspense>
+      <Suspense fallback={<HourlySkeleton />}>
+        <HourlyForecast coords={coords} />
+      </Suspense>
+      <Suspense fallback={<DailySkeleton />}>
+        <DailyForecast coords={coords} />
+      </Suspense>
+      <Suspense fallback={<AdditionalInfoSkeleton />}>
+        <AdditionalInfo coords={coords} />
+      </Suspense>
     </div>
   );
 }
